832190de32b4c3d97dca5e7ae261d260630bfb88
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-08-16 18:06:17 -0500

    Puppy training

---
+++ adaption_site.py
@@ -0,0 +1,87 @@
+import os 
+from forms import AddForm, DelForm
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'app_secret'
+
+#####################################
+###### SQL DBA SECTION ##############
+#####################################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+DB = SQLAlchemy(app)
+app.app_context().push()
+Migrate(app, DB)
+
+#####################################
+###### SQL TABLE ORM ################
+#####################################
+
+class Puppy(DB.Model):
+    
+    __tablename__ = 'puppies'
+    
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.Text)
+    
+    def __init__(self, name):
+        self.name = name 
+        
+    def __repr__(self):
+        return f"Puppy name: {self.name}"
+    
+
+#####################################
+####### VIEW FUNCTIONS - HTML FORMS #
+#####################################
+
+@app.route('/')
+def index():
+    welcome_message = 'Hello and Welcome!'
+    return render_template('home.html',welcome_message=welcome_message)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pup():
+    
+    form = AddForm()
+    if form.validate_on_submit():
+        name = form.name.data 
+        
+        #DB Model
+        new_pup = Puppy(name)
+        DB.session.add(new_pup)
+        DB.session.commit()
+        return redirect(url_for('list_pup'))
+    
+    return render_template('add.html', form=form)
+
+
+@app.route('/list')
+def list_pup():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+
+@app.route('/delete', methods=['GET','POST'])
+def del_pup():
+    form = DelForm()
+    if form.validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        DB.session.delete(pup)
+        DB.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('delete.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+
+class AddForm(FlaskForm):
+    
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Submit Puppy')
+
+class DelForm(FlaskForm):
+    id = IntegerField('ID of Puppy: ')
+    submit = SubmitField('Remove Puppy')
+    
+    
+    
+    
